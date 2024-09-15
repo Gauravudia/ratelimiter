0a1d6aa4b4405aead5f6a05052bbb592b3972cbd
Gauravudia <gauravudia@rudderstack.com>
2024-09-15 23:02:41 +0530

    fix: add synchronization and use refill Rate Per Millis for continuous token filling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,28 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         // refill after every minute, timeWindowMillis: 60000
         RateLimiter rateLimiter = new TokenBucketRateLimiter(3, 60000);
-        for (int i = 0; i < 10; i++) {
-            rateLimiter.tryAcquire();
-            Thread.sleep(10000);
+        Thread[] threads = new Thread[10];
+        for(int i=0;i<10;i++){
+            threads[i] = new Thread(()->{
+                try {
+                    rateLimiter.tryAcquire();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(Thread.currentThread().getName() + " was interrupted.");
+                }
+                System.out.println(Thread.currentThread().getName() + " has finished!");
+            });
+            threads[i].start();
         }
+
+        for(int i=0;i<10;i++){
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                System.out.println("Main thread interrupted.");
+            }
+        }
+
+        System.out.println("All threads have finished. Main thread is terminating.");
     }
 }
--- src/main/java/org/example/TokenBucketRateLimiter.java
+++ src/main/java/org/example/TokenBucketRateLimiter.java
@@ -2,31 +2,32 @@ package org.example;
 
 public class TokenBucketRateLimiter implements RateLimiter {
     private final long maxBucketSize;
-    private final long timeWindowMillis;
+    private final double refillRatePerMillis;
     private long currentBucketSize;
-    private long lastRefillTimestamp = System.currentTimeMillis();
+    private long lastRefillTimestamp;
 
     TokenBucketRateLimiter(long bucketSize, long timeWindowMillis){
-        this.maxBucketSize = bucketSize;
-        this.timeWindowMillis = timeWindowMillis;
+        maxBucketSize = bucketSize;
+        refillRatePerMillis = (double) bucketSize / timeWindowMillis;
         currentBucketSize = maxBucketSize;
+        lastRefillTimestamp = System.currentTimeMillis();
     }
 
     @Override
-    public boolean tryAcquire(){
+    public synchronized boolean tryAcquire(){
        refill();
        if(currentBucketSize >0){
            currentBucketSize--;
            System.out.println("Request accepted!");
            return true;
        }
-        System.out.println("Too many requests. Please try again.");
+       System.out.println("Too many requests. Please try again.");
        return false;
     }
 
     public void refill(){
         long currentTimeStamp = System.currentTimeMillis();
-        long noOfTokenToAdd = ((currentTimeStamp - lastRefillTimestamp)/ timeWindowMillis)* maxBucketSize;
+        long noOfTokenToAdd = (long) (Math.floor((currentTimeStamp - lastRefillTimestamp)* refillRatePerMillis));
         if(noOfTokenToAdd > 0){
             System.out.println("Refilled no of tokens: "+ noOfTokenToAdd);
             lastRefillTimestamp = currentTimeStamp;
